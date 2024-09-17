3e360500e4046de3c7a6954058f61df516f153f4
Danila Aliabjev <96951523+DanKb7@users.noreply.github.com>
2024-09-17 21:15:39 +0300

    Add files via upload

---
+++ Py_git/main.py
@@ -0,0 +1,18 @@
+#Пример для 232гр 
+class Add:
+    def __init__(self,a,b):
+        self.a = a 
+        self.b = b 
+
+    def Result(self): #сложение в ООП
+        return self.a + self.b 
+    
+class Ret(Add):# Умножение в ООП
+    def Prioriti(self):
+        return self.a * self.b
+
+a = Add(int(input("Введи переменную А: ")),int(input("Введи переменную А: ")))
+print("Ваш результат = ", a.Result())
+b = Ret(int(input("Введи переменную Б: ")),int(input("Введи переменную Б: ")))
+print("Ваш результат = ", b.Prioriti())
+
